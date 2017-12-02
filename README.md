c514875524b3fd0e93d7ac497d99ef44df5953ee
Abhinav S <abhinav.saxena@novoinvent.com>
2017-12-02 10:30:26 +0530

    Initial Commit

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
-# Project
-
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 1.5.5.
+# VR-Maps-Demo
+RH-3.0
 
 ## Development server
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { StateLandingComponent } from './state-landing/state-landing.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StateLandingComponent
   ],
   imports: [
     BrowserModule
